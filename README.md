9bddb990829cf935f8469e0535d3a7c847951cba
unknown <professorjacobian@gmail.com>
2024-12-06 06:23:51 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+echo # Proxy Server
+
+A simple proxy server to handle HTTPS to HTTP proxying.
+
+## Setup
+1. npm install
+2. npm start
+
+## Environment Variables
+- PORT (optional, defaults to 3000) > README.md
---
+++ server.js
@@ -0,0 +1,20 @@
+echo const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const proxyConfig = {
+    target: 'http://45.13.225.83:5000',
+    changeOrigin: true,
+    secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+};
+
+app.use('/', createProxyMiddleware(proxyConfig));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Proxy server running on port ${PORT}')); > server.js
