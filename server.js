75d246f9b27722032556b963bd2ffb6f061cc8c6
unknown <professorjacobian@gmail.com>
2024-12-06 06:58:30 -0800

    Update package.json and server.js

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ const proxyConfig = {
 app.use('/', createProxyMiddleware(proxyConfig));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Proxy server running on port ${PORT}')); > server.js
+app.listen(PORT, () => console.log('Proxy server running on port ' + PORT)); > server.js
